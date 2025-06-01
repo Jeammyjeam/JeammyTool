2da2255feb1589dc06d3d7572a5797ea0c295602
Jeammyjeam <142210037+Jeammyjeam@users.noreply.github.com>
2025-06-01 16:49:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-  import streamlit as st
-    x = st.slider('Select a value')
-    st.write(x, 'squared is', x * x)
-    
 
     git add app.py 
     git commit -m "Add application file"
     git push
+
+import streamlit as st
+    x = st.slider('Select a value')
+    st.write(x, 'squared is', x * x)
+    
+
+    g
